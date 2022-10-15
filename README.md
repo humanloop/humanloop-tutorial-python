f2718a0784e99df68950235ad7f71aedc052387a
Raza <rhabib01@gmail.com>
2022-10-15 17:27:13 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Humanloop API Quickstart - Python example app
+
+This is an example app that shows you how to use the Humanloop API in a GPT-3 app. It uses the [Flask](https://flask.palletsprojects.com/en/2.2.x/) web framework and [Humanloop](https://humanloop.com) for data logging and model improvement. Check out the tutorial or follow the instructions below to get set up.
+
+## Setup
+
+1. If you don’t have Python installed, [install it from here](https://www.python.org/downloads/)
+
+2. Clone this repository
+
+3. Navigate into the project directory
+
+   ```bash
+   $ cd openai-tutorial-python
+   ```
+
+4. Create a new virtual environment
+
+   ```bash
+   $ python -m venv venv
+   $ . venv/bin/activate
+   ```
+
+5. Install the requirements
+
+   ```bash
+   $ pip install -r requirements.txt
+   ```
+
+6. Make a copy of the example environment variables file
+
+   ```bash
+   $ cp .env.example .env
+   ```
+
+7. Add your [OpenAI API key](https://beta.openai.com/account/api-keys) and [Humanloop API key](https://app.humanloop.com/llama/settings) to the newly created `.env` file
+
+8. Run the app
+
+   ```bash
+   $ flask --debug run
+   ```
+
+You should now be able to access the app at [http://localhost:5000](http://localhost:5000)! For the full context behind this example app, check out the [tutorial](https://beta.openai.com/docs/quickstart).
---
+++ app.py
@@ -0,0 +1,105 @@
+import os
+import textwrap
+import os
+from uuid import uuid4
+
+import openai
+import humanloop as hl
+from flask import Flask, redirect, render_template, request, url_for
+
+from utils import generate_prompt_from_template
+
+
+app = Flask(__name__)
+
+hl.init(
+    api_key=os.getenv('HUMANLOOP_API_KEY'),
+    provider_api_keys={"OpenAI":os.getenv("OPENAI_API_KEY")}
+)
+
+
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template(
+        "index.html",
+        result=request.args.get("result"),
+        log_id=request.args.get("log_id"),
+        feedback=request.args.get("feedback"),
+        copied=request.args.get("copied", False),
+    )
+
+
+@app.route("/get-question", methods=["POST"])
+def get_question():
+    # Make the request to GPT-3
+    expert = request.form["Expert"]
+    topic = request.form["Topic"]
+
+    generation = hl.generate(
+        project="LearnAnythingFromAnyone",
+        inputs={
+            "expert": expert,
+            "topic": topic
+    }
+)
+    log_id = generation.logs[0].id
+    result = generation.logs[0].output
+
+    return redirect(url_for("index", result=result, log_id=log_id))
+
+
+@app.route("/actions/thumbs-up", methods=["POST"])
+def thumbs_up():
+    log_id = request.args.get("log_id")
+
+    # Send explicit feedback 👍 to Humanloop
+    hl.feedback(group="feedback", label="👍", log_id=log_id)
+
+    return redirect(
+        url_for(
+            "index",
+            result=request.args.get("result"),
+            log_id=log_id,
+            feedback="👍",
+            copied=request.args.get("copied", False),
+        )
+    )
+
+
+@app.route("/actions/thumbs-down", methods=["POST"])
+def thumbs_down():
+    log_id = request.args.get("log_id")
+
+    # Send explicit feedback 👎 to Humanloop
+    hl.feedback(group="feedback", label="👎", log_id=log_id)
+    print(f"Recorded 👎 feedback to log: {log_id}")
+
+    return redirect(
+        url_for(
+            "index",
+            result=request.args.get("result"),
+            log_id=log_id,
+            feedback="👎",
+            copied=request.args.get("copied", False),
+        )
+    )
+
+
+@app.route("/actions/copy", methods=["POST"])
+def feedback():
+    log_id = request.args.get("log_id")
+
+    # Send implicit feedback to Humanloop
+    hl.feedback(group="implicit", label="copy", log_id=log_id)
+
+    return redirect(
+        url_for(
+            "index",
+            result=request.args.get("result"),
+            log_id=log_id,
+            feedback=request.args.get("feedback"),
+            copied=True,
+        )
+    )
