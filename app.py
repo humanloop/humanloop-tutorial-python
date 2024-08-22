82f0e1018e392fc933be464ba5eab5e78476cb0d
gavinfd-hl <gavin@humanloop.com>
2024-08-22 17:22:25 +0100

    Updating the project to use the v5 humanloop endpoints

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 import os
 
-from humanloop import Humanloop
+from humanloop import Humanloop, ProviderApiKeysParams
 from flask import Flask, redirect, render_template, request, url_for
+from dotenv import load_dotenv
 
+
+load_dotenv()
 app = Flask(__name__)
 
 HUMANLOOP_API_KEY = os.getenv("HUMANLOOP_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-humanloop = Humanloop(
-    api_key=HUMANLOOP_API_KEY,
-)
+hl = Humanloop(api_key=HUMANLOOP_API_KEY)
 
 
 @app.route("/", methods=["GET"])
@@ -18,45 +19,64 @@ def index():
     return render_template(
         "index.html",
         result=request.args.get("result"),
-        data_id=request.args.get("data_id"),
-        feedback=request.args.get("feedback"),
-        copied=request.args.get("copied", False),
+        log_id=request.args.get("log_id"),
+        evaluation=request.args.get("evaluation"),
     )
 
 
 @app.route("/get-question", methods=["POST"])
 def get_question():
-    # Make the request to GPT-3
+    # Make the request to GPT-4
     expert = request.form["Expert"]
     topic = request.form["Topic"]
 
-    # hl.complete automatically logs the data to your project.
-    complete_response = humanloop.complete_deployed(
-        project="learn-anything",
+    # hl.prompts.call automatically logs the data to your prompt.
+    call_response = hl.prompts.call(
+        path="learn-anything",
         inputs={"expert": expert, "topic": topic},
-        provider_api_keys={"openai": OPENAI_API_KEY},
+        prompt={
+            "template": [
+                {
+                    "role": "system",
+                    "content": "You are {{expert}}. Write a joke about {{topic}}.",
+                }
+            ],
+            "model": "gpt-4",
+        },
+        provider_api_keys=ProviderApiKeysParams(openai=OPENAI_API_KEY),
     )
-    data_id = complete_response.body["data"][0]["id"]
-    result = complete_response.body["data"][0]["output"]
 
-    print("data_id from completion: ", data_id)
-    return redirect(url_for("index", result=result, data_id=data_id))
+    # The log_id is the ID of the log on Humanloop that was created by the call
+    log_id = call_response.id
+    log_response = call_response.logs[0]
+
+    return redirect(url_for("index", result=log_response.output, log_id=log_id))
 
 
 @app.route("/actions/thumbs-up", methods=["POST"])
 def thumbs_up():
-    data_id = request.args.get("data_id")
+    log_id = request.args.get("log_id")
 
-    # Send rating feedback to Humanloop
-    humanloop.feedback(type="rating", value="good", data_id=data_id)
-    print(f"Recorded 👍 feedback to datapoint: {data_id}")
+    # We fetch the log from Humanloop to find which prompt it's associated with
+    log = hl.logs.get(id=log_id)
+    prompt_id = log.prompt.id
+
+    # We fetch the prompt to find which evaluator it's associated with
+    prompt = hl.prompts.get(id=prompt_id)
+
+    # Note this will be empty if you haven't assigned a monitoring evaluator to your prompt
+    evaluator_id = prompt.evaluators[0].version_reference.file.id
+
+    # Send rating evaluation to Humanloop using the evaluator_id and log_id
+    hl.evaluators.log(parent_id=log_id, id=evaluator_id, judgment="2")
+    print(f"Recorded 👍 evaluation to log: {log_id}")
 
     return redirect(
         url_for(
             "index",
             result=request.args.get("result"),
-            data_id=data_id,
-            feedback="👍",
+            log_id=log_id,
+            evaluation="👍",
             copied=request.args.get("copied", False),
         )
     )
@@ -64,37 +84,28 @@ def thumbs_up():
 
 @app.route("/actions/thumbs-down", methods=["POST"])
 def thumbs_down():
-    data_id = request.args.get("data_id")
+    log_id = request.args.get("log_id")
 
-    # Send rating feedback to Humanloop
-    humanloop.feedback(type="rating", value="bad", data_id=data_id)
-    print(f"Recorded 👎 feedback to datapoint: {data_id}")
+    # We fetch the log from Humanloop to find which prompt it's associated with
+    log = hl.logs.get(id=log_id)
+    prompt_id = log.prompt.id
+
+    # We fetch the prompt to find which evaluator it's associated with
+    prompt = hl.prompts.get(id=prompt_id)
+
+    # Note this will be empty if you haven't assigned a monitoring evaluator to your prompt
+    evaluator_id = prompt.evaluators[0].version_reference.file.id
+
+    # Send rating evaluation to Humanloop using the evaluator_id and log_id
+    hl.evaluators.log(parent_id=log_id, id=evaluator_id, judgment="1")
+    print(f"Recorded 👎 evaluation to log: {log_id}")
 
     return redirect(
         url_for(
             "index",
             result=request.args.get("result"),
-            data_id=data_id,
-            feedback="👎",
+            log_id=log_id,
+            evaluation="👎",
             copied=request.args.get("copied", False),
-        )
-    )
-
-
-@app.route("/actions/copy", methods=["POST"])
-def feedback():
-    data_id = request.args.get("data_id")
-
-    # Send implicit feedback to Humanloop
-    humanloop.feedback(type="action", value="copy", data_id=data_id)
-    print(f"Recorded implicit feedback that user copied to datapoint: {data_id}")
-
-    return redirect(
-        url_for(
-            "index",
-            result=request.args.get("result"),
-            data_id=data_id,
-            feedback=request.args.get("feedback"),
-            copied=True,
         )
     )
